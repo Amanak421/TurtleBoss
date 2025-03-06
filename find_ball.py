0b853d52424d9f46cb45f6e9c3d1efd2b27002b4
Levap11 <janapav1@fel.cvut.cz>
2025-03-06 01:01:08 +0100

    Mask all obstacles

--- find_ball.py
+++ find_ball.py
@@ -7,13 +7,17 @@ LOWER_YELLOW = np.array([20, 100, 100])
 UPPER_YELLOW = np.array([30, 255, 255])
 
 # Blue, Green, Red
-LOWER_OBSTACLES = np.array([100, 50, 157], [40, 50, 50], [0, 75, 50])
-UPPER_OBSTACLES = np.array([140, 140, 157], [90, 255, 255], [10, 255, 255])
+LOWER_OBSTACLES = np.array([[90, 100, 50], [40, 50, 50], [0, 75, 50]])
+UPPER_OBSTACLES = np.array([[130, 255, 255], [90, 255, 255], [10, 255, 255]])
 
 """
 Modrá (H: 100–140)
 Min: H = 100, S = 50, V = 50
 Max: H = 140, S = 140, V = 157
+____________________________
+O 5 % méně podobné: H = 93, S = 242, V = 112
+O 5 % více podobné: H = 102, S = 255, V = 123
+____________________________
 
 Zelená (H: 40–90)
 Min: H = 40, S = 50, V = 50
@@ -23,9 +27,9 @@ Max: H = 90, S = 255, V = 255
 Min: H = 0, S = 50, V = 50
 Max: H = 10, S = 255, V = 255
 
-Červená (druhý rozsah, H: 170–180)
-Min: H = 170, S = 50, V = 50
-Max: H = 179, S = 145, V = 153
+#Červená (druhý rozsah, H: 170–180)
+#Min: H = 170, S = 50, V = 50
+#Max: H = 179, S = 145, V = 153
 
 
 
@@ -57,26 +61,31 @@ def find_ball(rgb_img, lower_y = LOWER_YELLOW, upper_y = UPPER_YELLOW) -> tuple 
         return None
 
 #TODO
-def find_obstacles(rgb_img, lower_y = LOWER_YELLOW, upper_y = UPPER_YELLOW) -> tuple | None:
-    # Convert to HSV
-    hsv = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2HSV)
-    mask = cv2.inRange(hsv, lower_y, upper_y)
+def find_obstacles(rgb_img, lower_o = LOWER_OBSTACLES, upper_o = UPPER_OBSTACLES) -> tuple | None:
+    obstacles_dict = {}
+    for i in range(np.size(lower_o, 0)):
 
-    contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        # Convert to HSV
+        hsv = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lower_o[i], upper_o[i])
 
-    if contours:
-        largest_contour = max(contours, key=cv2.contourArea)
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        min_area = 500
+        
 
-        (x, y), radius = cv2.minEnclosingCircle(largest_contour)
-        center = (int(x), int(y))
-        radius = int(radius)
-
-        draw_circle(rgb_img, center, radius)
-
-        return center, radius
-    else:
-        return None
-
+        if contours:
+            for cnt in contours:
+                area = cv2.contourArea(cnt)
+                if area > min_area:
+                    x, y, w, h = cv2.boundingRect(cnt)  # Obdélník okolo objektu
+                    if i not in obstacles_dict:
+                        obstacles_dict[i] = np.array([x, y, w, h])
+                    else:
+                        obstacles_dict[i].append([x, y, w, h])
+            #draw_rectangles(rgb_img, obstacles_dict)
+            return obstacles_dict
+        else:
+            return None
 
 def draw_circle(rgb_img, center, r) -> None:
     rgb_img = np.array(rgb_img, dtype=np.uint8)
@@ -85,12 +94,15 @@ def draw_circle(rgb_img, center, r) -> None:
     cv2.circle(rgb_img, center, 5, (0, 0, 255), -1)
     print(f"Center: {center}, radius: {r}")
     
-
-    #cv2.imshow("Win HSV", hsv)
     cv2.imshow("Win RGB", rgb_img)
-    #cv2.imshow("Win MASK", mask)
     cv2.waitKey()
     cv2.destroyAllWindows() 
+
+def draw_rectangles(rgb_img, obstacles_dict) -> None:#TODO
+    rgb_img = np.array(rgb_img, dtype=np.uint8)
+    rgb_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2BGRA)##FIXME###????
+    
+    
 
 
 def load_img(filename):
@@ -98,7 +110,18 @@ def load_img(filename):
     rgb_img = data["image_rgb"]
     return rgb_img
 
+def find_objects(rgb_img):
+    center, radius = find_ball(rgb_img)
+    
+
+
+    cv2.imshow("Win RGB obstacles", rgb_img)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
 for i in range(1,6):
     # Read .mat file
     rgb_img = load_img(f"test_data/test_y{i}.mat")
     find_ball(rgb_img)
+    find_obstacles(rgb_img)
