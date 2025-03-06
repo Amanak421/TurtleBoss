a1b86903e26e0ad728cc7516d581ec9ab695e4fc
NolaacVivnil <engiguin@gmail.com>
2025-03-06 16:22:41 +0100

    Change

--- find_ball.py
+++ find_ball.py
@@ -2,12 +2,40 @@ import cv2
 import numpy as np
 import scipy.io
 
-
 LOWER_YELLOW = np.array([20, 100, 100])
 UPPER_YELLOW = np.array([30, 255, 255])
 
+# Blue, Green, Red
+LOWER_OBSTACLES = np.array([[90, 100, 50], [40, 50, 50], [0, 75, 50]])
+UPPER_OBSTACLES = np.array([[130, 255, 255], [90, 255, 255], [10, 255, 255]])
 
-def find_ball(rgb_img, lower_y = LOWER_YELLOW, upper_y = UPPER_YELLOW) -> tuple | None:
+"""
+Modrá (H: 100–140)
+Min: H = 100, S = 50, V = 50
+Max: H = 140, S = 140, V = 157
+____________________________
+O 5 % méně podobné: H = 93, S = 242, V = 112
+O 5 % více podobné: H = 102, S = 255, V = 123
+____________________________
+
+Zelená (H: 40–90)
+Min: H = 40, S = 50, V = 50
+Max: H = 90, S = 255, V = 255
+
+Červená (první rozsah, H: 0–10)
+Min: H = 0, S = 50, V = 50
+Max: H = 10, S = 255, V = 255
+
+#Červená (druhý rozsah, H: 170–180)
+#Min: H = 170, S = 50, V = 50
+#Max: H = 179, S = 145, V = 153
+
+
+
+"""
+
+
+def find_ball(rgb_img, lower_y=LOWER_YELLOW, upper_y=UPPER_YELLOW) -> tuple | None:
     # Convert to HSV
     hsv = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_y, upper_y)
@@ -20,28 +48,103 @@ def find_ball(rgb_img, lower_y = LOWER_YELLOW, upper_y = UPPER_YELLOW) -> tuple 
         (x, y), radius = cv2.minEnclosingCircle(largest_contour)
         center = (int(x), int(y))
         radius = int(radius)
-
-        draw_circle(rgb_img, center, radius)
-
         return center, radius
     else:
         return None
 
 
+# TODO
+def find_obstacles(rgb_img, lower_o=LOWER_OBSTACLES, upper_o=UPPER_OBSTACLES) -> tuple | None:
+    obstacles_dict = {}
+    for i in range(np.size(lower_o, 0)):
+
+        # Convert to HSV
+        hsv = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2HSV)
+        mask = cv2.inRange(hsv, lower_o[i], upper_o[i])
+
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        min_area = 500
+
+        if contours:
+            for cnt in contours:
+                area = cv2.contourArea(cnt)
+                if area > min_area:
+                    x, y, w, h = cv2.boundingRect(cnt)  # Obdélník okolo objektu
+                    if i not in obstacles_dict:
+                        obstacles_dict[i] = [[x, y, w, h]]
+                    else:
+                        obstacles_dict[i].append([x, y, w, h])
+
+    return obstacles_dict
+
+
 def draw_circle(rgb_img, center, r) -> None:
-    # Kreslení detekovaného míče
-    cv2.circle(rgb_img, center, r, (0, 255, 0), 3)
+    rgb_img = np.array(rgb_img, dtype=np.uint8)
+    rgb_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2BGRA)  ##FIXME###????
+    cv2.circle(rgb_img, center, r, (0, 255, 255), 3)
     cv2.circle(rgb_img, center, 5, (0, 0, 255), -1)
-    print(f"Center: {center}, radius: {r}")
+    # print(f"Center: {center}, radius: {r}")
+    return rgb_img
 
-    #cv2.imshow("Win HSV", hsv)
-    cv2.imshow("Win RGB", rgb_img)
-    #cv2.imshow("Win MASK", mask)
+
+def draw_rectangles(rgb_img, obstacles_dict) -> None:
+    rgb_img = np.array(rgb_img, dtype=np.uint8)
+    rgb_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2BGRA)
+    color_arr = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
+    for key in obstacles_dict:
+        for rec in obstacles_dict[key]:
+            [x, y, w, h] = rec
+            cv2.rectangle(rgb_img, (x, y), (x + w, y + h), color_arr[key], 2)
+    return rgb_img
+
+
+def draw_center(rgb_img, center_dict) -> None:
+    rgb_img = np.array(rgb_img, dtype=np.uint8)
+    rgb_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2BGRA)
+    for key in center_dict:
+        for c in center_dict[key]:
+            [x, y] = c
+            cv2.circle(rgb_img, (x, y), 5, (0, 0, 255), -1)
+    return rgb_img
+
+
+def show_objects(rgb_img, center, radius, obstacles_dict, obstacles_center_dict) -> None:
+    rgb_img = draw_circle(rgb_img, center, radius)
+    rgb_img = draw_rectangles(rgb_img, obstacles_dict)
+    rgb_img = draw_center(rgb_img, obstacles_center_dict)
+
+    cv2.imshow("RGB all objects", rgb_img)
     cv2.waitKey()
-    cv2.destroyAllWindows() 
+    cv2.destroyAllWindows()
 
-for i in range(1,9):
+
+def determine_center(obstacles_dict) -> dict:
+    center_dict = {}
+    for key in obstacles_dict:
+        for rec in obstacles_dict[key]:
+            [x, y, w, h] = rec
+            if key not in center_dict:
+                center_dict[key] = [[x + w // 2, y + h // 2]]
+            else:
+                center_dict[i].append([x + w // 2, y + h // 2])
+    return center_dict
+
+
+def load_img(filename):
+    data = scipy.io.loadmat(filename)
+    rgb_img = data["image_rgb"]
+    return rgb_img
+
+
+def find_objects(rgb_img):
+    center, radius = find_ball(rgb_img)
+    obstacles_dict = find_obstacles(rgb_img)
+    obstacles_center_dict = determine_center(obstacles_dict)
+    show_objects(rgb_img, center, radius, obstacles_dict, obstacles_center_dict)
+    return center, obstacles_center_dict
+
+
+for i in range(1, 9):
     # Read .mat file
-    data = scipy.io.loadmat(f"test_data/test_y{i}.mat")
-    rgb_img = data["K_rgb"]
-    find_ball(rgb_img)
+    rgb_img = load_img(f"test_data/test_y{i}.mat")
+    find_objects(rgb_img)
