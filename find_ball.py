55288ec2b71929ddceac063f238ce5b1ac820cf3
Levap11 <janapav1@fel.cvut.cz>
2025-03-09 17:42:00 +0100

    Find object visualization

--- find_ball.py
+++ find_ball.py
@@ -40,14 +40,13 @@ def find_ball(rgb_img, all_objects, lower_y=LOWER_YELLOW, upper_y=UPPER_YELLOW) 
 
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     if contours:
-        largest_contour = max(contours, key=cv2.contourArea)
-        if largest_contour > MIN_AREA:
-            (x, y), radius = cv2.minEnclosingCircle(largest_contour)
+        largest_c = max(contours, key=cv2.contourArea)
+        if cv2.contourArea(largest_c) > MIN_AREA:
+            (x, y), radius = cv2.minEnclosingCircle(largest_c)
             all_objects.append(RigidObject(int(x), int(y), int(radius), int(radius), RigidType.BALL))
 
 
 def find_obstacles(rgb_img, all_objects, lower_o=LOWER_OBSTACLES, upper_o=UPPER_OBSTACLES) -> None:
-    obstacles_dict = {}
     for i in range(np.size(lower_o, 0)):
         # Convert to HSV
         hsv = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2HSV)
@@ -65,36 +64,34 @@ def find_obstacles(rgb_img, all_objects, lower_o=LOWER_OBSTACLES, upper_o=UPPER_
                     all_objects.append(RigidObject(int(cx), int(cy), int(w), int(h), r_type))
 
 
-def draw_circle(rgb_img, center, r) -> None:
+def draw_circle(rgb_img, x, y, r):
     rgb_img = np.array(rgb_img, dtype=np.uint8)
     rgb_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2BGRA)
-    cv2.circle(rgb_img, center, r, (0, 255, 255), 3)
-    cv2.circle(rgb_img, center, 5, (0, 0, 255), -1)
-    # print(f"Center: {center}, radius: {r}")
+    cv2.circle(rgb_img, (x, y), r, (0, 255, 255), 3)
+    cv2.circle(rgb_img, (x, y), 3, (0, 0, 255), -1)
     return rgb_img
 
 
-def draw_rectangles(rgb_img, obstacles_dict) -> None:
+def draw_rectangle(rgb_img, obst):
     rgb_img = np.array(rgb_img, dtype=np.uint8)
     rgb_img = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2BGRA)
-    color_arr = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
-    for key in obstacles_dict:
-        for rec in obstacles_dict[key]:
-            [x, y, w, h] = rec
-            cv2.rectangle(rgb_img, (x, y), (x + w, y + h), color_arr[key], 2)
+    x1, y1 = int(obst.x - obst.w // 2), int(obst.y - obst.h // 2)
+    x2, y2 = int(obst.x + obst.w // 2), int(obst.y + obst.h // 2)
+    r_color = (255, 0, 0) if obst.o_type == RigidType.POLE else (0, 0, 255)
+    cv2.rectangle(rgb_img, (x1, y1), (x2, y2), r_color, 2)
+    cv2.circle(rgb_img, (obst.x, obst.y), 3, (0, 0, 255), -1)
     return rgb_img
 
 
-
-def show_objects(rgb_img, center, radius, obstacles_dict) -> None:
-    rgb_img = draw_circle(rgb_img, center, radius)
-    rgb_img = draw_rectangles(rgb_img, obstacles_dict)
-    # rgb_img = draw_center(rgb_img, obstacles_center_dict)
-
+def show_objects(rgb_img, all_objects) -> None:
+    for obj in all_objects:
+        if obj.o_type == RigidType.BALL:
+            rgb_img = draw_circle(rgb_img, obj.x, obj.y, obj.w)
+        else:
+            rgb_img = draw_rectangle(rgb_img, obj)
     cv2.imshow("RGB all objects", rgb_img)
     cv2.waitKey()
-    cv2.destroyAllWindows()
-
+    
 
 def load_img(filename):
     data = scipy.io.loadmat(filename)
@@ -106,11 +103,13 @@ def find_objects(rgb_img):
     all_objects = []
     find_ball(rgb_img, all_objects)
     find_obstacles(rgb_img, all_objects)
-    show_objects(rgb_img, center, radius, obstacles_dict)
+    show_objects(rgb_img, all_objects)
     return all_objects
 
 
 if __name__ == "__main__":
     for img_index in range(1, 9):
         rgb_img_ = load_img(f"test_data/test_y{img_index}.mat")
-        find_objects(rgb_img_)
+        all_objects = find_objects(rgb_img_)
+        print(all_objects)
+    cv2.destroyAllWindows()
