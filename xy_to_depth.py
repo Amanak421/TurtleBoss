40dadf516b4ab13561e3e54ff1efe82f39403e96
NolaacVivnil <engiguin@gmail.com>
2025-03-10 11:50:41 +0100

    Show real photo

--- xy_to_depth.py
+++ xy_to_depth.py
@@ -1,13 +1,24 @@
-import numpy as np
-import scipy.io
+import find_ball
+import cv2
+from robolab_turtlebot import Turtlebot, sleep
 
-
-def load_pc(filename):
+"""def load_pc(filename):
     data = scipy.io.loadmat(filename)
     pc = data["point_cloud"]
-    return pc
+    return pc"""
+
+
 
 
 if __name__ == "__main__":
-    pc_ = load_pc(f"test_data/test_y{1}.mat")
-    pass
+    # pc_ = load_pc(f"test_data/test_y{1}.mat")
+    turtle = Turtlebot(rgb=True, depth=True, pc=True)
+    sleep(2)
+
+    for img_index in range(5):
+        turtle.wait_for_rgb_image()
+        rgb_img_ = turtle.get_rgb_image()
+        all_objects_ = find_ball.find_objects(rgb_img_)
+        print(all_objects_)
+    cv2.destroyAllWindows()
+
