3f144522459366c8163015c02ec6a35cd1fe4b72
NolaacVivnil <engiguin@gmail.com>
2025-03-10 13:11:40 +0100

    Show real photo update

--- xy_to_depth.py
+++ xy_to_depth.py
@@ -2,16 +2,8 @@ import find_ball
 import cv2
 from robolab_turtlebot import Turtlebot, sleep
 
-"""def load_pc(filename):
-    data = scipy.io.loadmat(filename)
-    pc = data["point_cloud"]
-    return pc"""
-
-
-
 
 if __name__ == "__main__":
-    # pc_ = load_pc(f"test_data/test_y{1}.mat")
     turtle = Turtlebot(rgb=True, depth=True, pc=True)
     sleep(2)
     window_ = "RGB all objects"
@@ -22,6 +14,19 @@ if __name__ == "__main__":
         rgb_img_ = turtle.get_rgb_image()
         all_objects_ = find_ball.find_objects(rgb_img_)
         print(*all_objects_, sep="\n")
-        find_ball.show_objects(rgb_img_, all_objects_, window_, wait=False)
+
+        ball = list(filter(lambda x: x.o_type == find_ball.RigidType.BALL, all_objects_))
+        if ball:
+            ball = ball[0]
+            pc = turtle.get_point_cloud()
+            if not pc:
+                print('No point cloud')
+                continue
+            else:
+                print("yx", pc[ball.y][ball.x])
+                print("xy", pc[ball.x][ball.y])
+
+        find_ball.show_objects(rgb_img_, all_objects_, window_, wait=True)
+
     cv2.destroyAllWindows()
 
