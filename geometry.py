15bd71917f1725f545c69ea298fface3e8b36c2f
NolaacVivnil <engiguin@gmail.com>
2025-03-20 14:55:26 +0100

    segment, circle intersection

--- geometry.py
+++ geometry.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def normalize_angle(angle):
         return np.arctan2(np.sin(angle), np.cos(angle))
 
@@ -72,14 +73,18 @@ class Segment:
         c = nv.x * self.a.x + nv.y * self.a.y
         return np.array((nv.x, nv.y, c))
 
-    def is_element_of(self, point: Point):
-        pass  # TODO
+    def is_element_of(self, point: Point, atol=1e-9):
+        cross_product = (point.y - self.a.y) * (self.b.x - self.a.x) - (self.b.y - self.a.y) * (point.x - self.a.x)
+        collinear = cross_product < atol
+        within_bounds = (min(self.a.x, self.b.x) - atol <= point.x <= max(self.a.x, self.b.x) + atol and
+                         min(self.a.y, self.b.y) - atol <= point.y <= max(self.a.y, self.b.y) + atol)
+        return collinear and within_bounds
 
 
 
 class Circle:
-    def __init__(self, s: Point, r: float):
-        self.s, self.r = s, r
+    def __init__(self, c: Point, r: float):
+        self.c, self.r = c, r
 
 
 def intersection(circle: Circle, segment: Segment):
@@ -87,18 +92,23 @@ def intersection(circle: Circle, segment: Segment):
     dx = segment.direction_vector.x
     dy = segment.direction_vector.y
     dr = np.sqrt(np.sum(np.square(dx), np.square(dy)))
-    d = np.linalg.det(((segment.a.x, segment.b.x), (segment.a.y, segment.b.y)))
+    d = np.linalg.det(((segment.a.x - circle.c.x, segment.b.x - circle.c.x), (segment.a.y - circle.c.y, segment.b.y - circle.c.y)))
     delta = np.square(circle.r) * np.square(dr) - np.square(d)
     if delta < 0:
-        return None
+        return []
     else:
         sign_dy = -1 if dy < 0 else 1
         x1 = (d * dy + sign_dy * dx * np.sqrt(delta)) / np.square(dr)
         y1 = (-d * dx + abs(dy) * np.sqrt(delta)) / np.square(dr)
         x2 = (d * dy - sign_dy * dx * np.sqrt(delta)) / np.square(dr)
         y2 = (-d * dx + abs(dy) * np.sqrt(delta)) / np.square(dr)
-        return Point(x1, y1), Point(x2, y2)
+        p1 = Point(x1 + circle.c.x, y1 + circle.c.y)
+        p2 = Point(x2 + circle.c.x, y2 + circle.c.y)
+        intersects = []
+        if segment.is_element_of(p1): intersects.append(p1)
+        if segment.is_element_of(p2): intersects.append(p2)
+        return intersects
 
 
 if __name__ == "__main__":
-    print(intersection(Circle(Point(1, 0), 5), Segment(Point(6, 3), Point(-6, 3))))
+    print(intersection(Circle(Point(0, 1), 5), Segment(Point(6, 4), Point(-2, 4))))
