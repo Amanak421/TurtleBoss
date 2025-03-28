a0ff2e9eb7c7aa3df24d994d9e7f3eb0dc6a1700
Jakub <kub.and123@gmail.com>
2025-03-28 10:33:36 +0100

    new_test_data

---
+++ examples/example_photo.py
@@ -0,0 +1,39 @@
+import sys
+from math import pi
+from robolab_turtlebot import Turtlebot, sleep, Rate, get_time
+import find_ball
+from mapping import Map
+from robot import Robot
+from geometry import Point
+from scipy.io import savemat
+from datetime import datetime
+
+if __name__ == "__main__":
+    turtle_ = Turtlebot(rgb=True, depth=True, pc=True)
+    sleep(2)
+    turtle_.play_sound(1)
+    sleep(0.3)
+    rate = Rate(50)
+    robot = Robot(turtle_, rate)
+    robot.reset()
+    robot_map = Map()
+    
+    while not turtle_.is_shutting_down():
+        if robot.button:
+            turtle_.play_sound(5)
+            # get K, images, and point cloud -> copy from example scripts
+            data = dict()
+            data['K_rgb'] = turtle_.get_rgb_K()
+            data['K_depth'] = turtle_.get_depth_K()
+            data['image_rgb'] = turtle_.get_rgb_image()
+            data['image_depth'] = turtle_.get_depth_image()
+            data['point_cloud'] = turtle_.get_point_cloud()
+
+            # save data to .mat file
+            filename = datetime.today().strftime("photos/%Y-%m-%d-%H-%M-%S") + ".mat"
+            savemat(filename, data)
+
+            print('Data saved in {}'.format(filename))
+            robot.button = False
+
+
